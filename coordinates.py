2baad8b55f405fede29c4c7943b80520044b2b84
HumayounKhan09 <humayounkhan09@outlook.com>
2026-01-27 15:11:14 -0500

    firdt push

---
+++ coordinates.py
@@ -0,0 +1,22 @@
+"""
+File Name: Notes.py
+implements the notes class
+"""
+class coordinates:
+    def ___init__(self):
+        pass
+
+    def update(self, x:int, y:int):
+        self.x = x
+        self.y = y
+
+        return x,y
+    
+    def set_coordinates(self,x:int, y:int):
+        self.x= x
+        self.y = y
+    
+    
+    
+
+
---
+++ note.py
@@ -0,0 +1,10 @@
+from coordinates import coordinates
+class note:
+    def __init__(self,colour: str, message: str, is_pinned: bool):
+        self.colour = colour
+        self.message = message
+        self.pinned = is_pinned
+
+    
+    
+
