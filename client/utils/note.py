770586251db0e574241d1b2131eb0f237b2afb8e
Mohammad El-Hassan <mohammadelhsn@gmail.com>
2026-01-28 14:11:14 -0500

    the ui for a pin

--- client/utils/note.py
+++ client/utils/note.py
@@ -1,10 +1,39 @@
 from client.utils.coordinates import coordinates
-class note:
-    def __init__(self,colour: str, message: str, is_pinned: bool):
+from graphics import *
+from pins import Pin
+from typing import List
+# GUI
+
+
+class Note(Button):
+    def __init__(self, colour: str, message: str, is_pinned: bool, P1: Point, P2: Point, win: GraphWin):
         self.colour = colour
         self.message = message
         self.pinned = is_pinned
+        self.pins: List[Pin] = []
+        super().__init__(P1, P2, message, win)
+        self.setButtonFill(colour)
 
-    
-    
+    def pin(self, p: Pin):
+        self.pins.append(p)
+        self.pinned = True
+        return
 
+    def unpin(self, p: Pin):
+        if (len(self.pins) == 0):
+            return
+        if p in self.pins:
+            self.pins.remove(p)
+            p.undraw()
+
+        if (len(self.pins) == 0):
+            self.pinned = False
+            self.undraw()
+
+
+"""
+Usage: note = Note(color, message, is_pinned, Point(x1,y1), Point(x2,y2), win)
+Extends Button so that when users click it unpins? 
+
+When a user clicks on the Note self.clicked === TRUE
+"""
--- client/utils/pins.py
+++ client/utils/pins.py
@@ -1,2 +1,13 @@
 
-from client.utils.coordinates import coordinates 
+from client.utils.coordinates import coordinates
+from graphics import *
+
+
+class Pin(Circle):
+    def __init__(self, center: Point, radius: int):
+        super().__init__(center, radius)
+
+
+"""
+Usage: pin = Pin(Point(x1, y1),  5)
+"""
