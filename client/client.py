9379e0104075e5fa887e7c002637ae6dcac4c341
Mohammad El-Hassan <mohammadelhsn@gmail.com>
2026-02-04 14:35:23 -0500

    Update client.py

--- client/client.py
+++ client/client.py
@@ -5,6 +5,7 @@ from graphics import *
 DEF_HOST = ''  # 127.0.0.1?
 DEF_PORT = 9000
 BUFFER = 1024
+UI_STEP = 60
 
 
 def start_client():
@@ -15,22 +16,35 @@ def start_client():
 
     # Parse JSON for the data
     raw_settings = client.recv(BUFFER)
+
+    # If there's no data, just close the client
     if not raw_settings:
         print("No data")
         return
+
+    # Get the JSON config from the server
     boardWidth, boardHeight, noteHeight, noteWidth, colours = json.loads(
         raw_settings.decode('utf-8'))
+
+    #! Can be removed later
     print(f'Board w = {boardWidth} x h = {boardHeight}')
     print(f'Note  w = {noteWidth} x h = {noteHeight}')
-    print(f'{colours}')
 
-    win = GraphWin("TITLE GOES HERE", boardWidth, boardHeight)
-    # Define the GUI
+    # * Array of colours
+    arrColours = colours.split(' ')
+    print(f'{arrColours}')
+
+    # * The window for the GUI
+    win = GraphWin("CP372 - A01", boardWidth, boardHeight)
+
+    # * Define Buttons
     clearButton = Button(Point(20, 20), Point(100, 60), "CLEAR", win).draw()
-    disconnectButton = Button(
-        Point(20, 80), Point(100, 120), "DISCONNECT", win).setFontSize(8).draw()
+    disconnectButton = Button(Point(20, 80), Point(
+        100, 120), "DISCONNECT", win).setFontSize(8).draw()
+    shakeButton = Button(Point(20, 80 + UI_STEP),
+                         Point(100, 120+UI_STEP), "SHAKE", win)
 
-    # Define the input
+    # * Define the input
     portEntry = Entry(Point(boardWidth-50, 20), 10).draw(win)
     ipEntry = Entry(Point(boardWidth-50, 40), 10).draw(win)
 
@@ -39,9 +53,12 @@ def start_client():
         if (clearButton.clicked(x)):
             client.sendall("CLEAR".encode("utf-8"))
             pass
+        elif (shakeButton.clicked(x)):
+            client.sendall('SHAKE'.encode('utf-8'))
+            pass
         elif (disconnectButton.clicked(x)):
             client.sendall('DISCONNECT'.encode('utf-8'))
-            # disconnect
+            # Safe exit, need to make sure we close the thread !
             pass
 
 
