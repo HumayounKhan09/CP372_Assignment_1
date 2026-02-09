199b647f4fb5a9bcd09c80dc81b508a8bcb6c76d
Mohammad El-Hassan <mohammadelhsn@gmail.com>
2026-02-08 21:48:27 -0500

    demo

--- client/client.py
+++ client/client.py
@@ -2,7 +2,6 @@ import json
 import socket
 from graphics import *
 import argparse
-from queue import Queue
 from typing import List
 
 
@@ -12,7 +11,8 @@ class Pin(Circle):
 
 
 class Note(Button):
-    def __init__(self, colour: str, message: str, is_pinned: bool, P1: Point, P2: Point, win: GraphWin, id: str):
+    def __init__(self, colour: str, message: str, is_pinned: bool,
+                 P1: Point, P2: Point, win: GraphWin, id: str):
         self.colour = colour
         self.message = message
         self.pinned = is_pinned
@@ -20,153 +20,198 @@ class Note(Button):
         super().__init__(P1, P2, message, win)
         self.setButtonFill(colour)
 
-    """
-    Server Implementation
-    """
-
-    def pin(self, p: Pin):
-        self.pins.append(p)
-        self.pinned = True
-        return
-
-    def unpin(self, p: Pin):
-        if (len(self.pins) == 0):
-            return
-        if p in self.pins:
-            self.pins.remove(p)
-            p.undraw()
-
-        if (len(self.pins) == 0):
-            self.pinned = False
-            self.undraw()
-
 
 DEF_HOST = '0.0.0.0'
 DEF_PORT = 5000
 BUFFER = 1024
 UI_STEP = 60
-
-# * Sample commands
-# ? POST 2 3 white Meeting next Wednesday from 2 to 3
-# ? SHAKE
-# ? CLEAR
-# ? DISCONNECT
-# ? GET color=<color> contains=<x> <y> refersTo=<substring>
-# ? PIN <x> <y>
-# ? UNPIN <x> <y>
+PIN_RADIUS = 4
+PIN_CLICK_RADIUS2 = 12 * 12   # distance² tolerance
 
 
 def start_client(host, port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # ? Does this need to be a queue?
-    notes = []
-    # Connect to the server
     client.connect((host, port))
 
-    # Parse JSON for the data
-    raw_settings = client.recv(BUFFER)
+    notes: List[Note] = []
+    pins: List[Pin] = []
 
-    # If there's no data, just close the client
+    raw_settings = client.recv(BUFFER)
     if not raw_settings:
-        print("No data")
+        print("No server data")
         return
 
-    # Get the JSON config from the server
     boardWidth, boardHeight, noteHeight, noteWidth, colours = json.loads(
-        raw_settings.decode('utf-8'))
+        raw_settings.decode("utf-8")
+    )
 
-    #! Can be removed later
-    print(f'Board w = {boardWidth} x h = {boardHeight}')
-    print(f'Note  w = {noteWidth} x h = {noteHeight}')
+    print(f"Board: {boardWidth}x{boardHeight}")
+    print(f"Note:  {noteWidth}x{noteHeight}")
+
+    win = GraphWin("CP372 - A01", boardWidth, boardHeight)
+
+    def clear_pins():
+        for p in pins:
+            p.undraw()
+        pins.clear()
+
+    def redraw_pins():
+        clear_pins()
+
+        client.sendall(b"GET_PINS")
+        raw = client.recv(BUFFER).decode("utf-8").strip()
+
+        if raw.startswith("ERROR"):
+            return
+
+        lines = raw.splitlines()
+        if lines[0] != "OK" or lines[-1] != "END":
+            print("Malformed GET_PINS")
+            return
+
+        for line in lines[1:-1]:
+            try:
+                _, x, y = line.split()
+                x = int(x)
+                y = int(y)
+
+                p = Pin(Point(x, y), PIN_RADIUS)
+                p.setFill("black")
+                p.draw(win)
+                pins.append(p)
+            except ValueError:
+                print("Bad pin line:", line)
 
     def redraw_board():
         for n in notes:
             n.undraw()
         notes.clear()
 
-        # Re-fetch data
-        client.sendall("GET None None None None".encode('utf-8'))
-        raw = client.recv(BUFFER).decode('utf-8')
-        boardNotes = json.loads(raw)
+        client.sendall(b"GET None None None None")
+        raw = client.recv(BUFFER).decode("utf-8").strip()
 
-        for noteData in boardNotes:
-            P1 = Point(noteData["x"], noteData["y"])
-            P2 = Point(noteData["x"] + noteWidth, noteData["y"] + noteHeight)
-            note = Note(noteData["colour"], noteData["text"],
-                        noteData["pinned"], P1, P2, win, noteData["id"])
-            note.draw()
-            notes.append(note)
-
-    # * The window for the GUI
-    win = GraphWin("CP372 - A01", boardWidth, boardHeight)
-
-    # * Define Buttons
-    clearButton = Button(Point(20, 20), Point(100, 60), "CLEAR", win).draw()
-    disconnectButton = Button(Point(20, 80), Point(
-        100, 120), "DISCONNECT", win).setFontSize(8).draw()
-    shakeButton = Button(Point(20, 80 + UI_STEP),
-                         Point(100, 120+UI_STEP), "SHAKE", win)
-    mode = "NOTE"
-    toggleButton = Button(Point(20, 140), Point(
-        100, 180), f"Mode: {mode}", win).setFontSize(8).draw()
-    while True:
-        x = win.getMouse()
-        if (clearButton.clicked(x)):
-            client.sendall("CLEAR".encode("utf-8"))
-            redraw_board()
-            pass
-        elif (shakeButton.clicked(x)):
-            client.sendall('SHAKE'.encode('utf-8'))
-            redraw_board()
-            pass
-        elif (disconnectButton.clicked(x)):
-            client.sendall('DISCONNECT'.encode('utf-8'))
-            #! Safe exit, need to make sure we close the thread !
-
-            # * We can safely close the UI
-            win.close()
-            print('Closing the GUI....')
+        if raw.startswith("ERROR"):
+            print(raw)
             return
-        elif toggleButton.clicked(x):
+
+        lines = raw.splitlines()
+        if lines[0] != "OK" or lines[-1] != "END":
+            print("Malformed GET")
+            return
+
+        for line in lines[1:-1]:
+            try:
+                x, y, colour, message, pinned_str = line.split(
+                    ",", 4)  # note the 5th field
+                x = int(x)
+                y = int(y)
+                is_pinned = pinned_str.strip().lower() in (
+                    "true", "1", "yes")  # convert to bool
+
+                P1 = Point(x, y)
+                P2 = Point(x + noteWidth, y + noteHeight)
+
+                n = Note(colour, message, is_pinned, P1, P2, win, f"{x},{y}")
+                n.draw()
+                notes.append(n)
+            except ValueError:
+                print("Bad note line:", line)
+
+        redraw_pins()
+
+    # UI buttons
+    clearButton = Button(Point(20, 20), Point(120, 60), "CLEAR", win).draw()
+    disconnectButton = Button(Point(20, 80), Point(120, 120),
+                              "DISCONNECT", win).setFontSize(8).draw()
+    shakeButton = Button(Point(20, 140), Point(120, 180),
+                         "SHAKE", win).draw()
+    mode = "NOTE"
+    toggleButton = Button(Point(20, 200), Point(120, 240),
+                          f"Mode: {mode}", win).setFontSize(8).draw()
+
+    redraw_board()
+
+    while True:
+        click = win.getMouse()
+
+        if clearButton.clicked(click):
+            client.sendall(b"CLEAR")
+            client.recv(BUFFER)
+            redraw_board()
+
+        elif shakeButton.clicked(click):
+            client.sendall(b"SHAKE")
+            client.recv(BUFFER)
+            redraw_board()
+            for note in notes:
+                if (not note.pinned):
+                    note.undraw()
+
+        elif disconnectButton.clicked(click):
+            client.sendall(b"DISCONNECT")
+            client.recv(BUFFER)
+            win.close()
+            return
+
+        elif toggleButton.clicked(click):
             mode = "PIN" if mode == "NOTE" else "NOTE"
             toggleButton.setText(f"Mode: {mode}")
-            continue
-        else:
-            if (mode == "NOTE"):
-                # Create a new note
-                x_position, y_position = x.getX(), x.getY()
-                #! will have to enter through the terminal.
-                text = input("Enter note text: ")
 
-                # TODO: Needs to be verified somewhere.
-                # TODO: pretty simple if (color not in coloursArray)
-                colour = input("Enter a colour: ")
+        elif mode == "NOTE":
+            x = int(click.getX())
+            y = int(click.getY())
 
-                # TODO: Verify overlap, complete overlap xOfSomeExistingNote=newX and
-                # TODO: yOfSomeExistingNote=newY but should this be defined server side?
+            text = input("Enter note text: ")
+            colour = input("Enter colour: ")
 
-                # ? This is subject to change.
-                cmd = f'POST {x_position} {y_position} {colour} {text}'
+            client.sendall(f"POST {x} {y} {colour} {text}".encode())
+            client.recv(BUFFER)
+            redraw_board()
 
-                client.sendall(cmd.encode('utf-8'))
+        elif mode == "PIN":
+            temp = True
+            x = int(click.getX())
+            y = int(click.getY())
+
+            for n in notes:
+                if not n.clicked(click):
+                    continue
+
+                nx = int(n.p1.getX())
+                ny = int(n.p1.getY())
+
+                closest_pin = None
+                best_dist = PIN_CLICK_RADIUS2
+
+                for p in pins:
+                    px = int(p.getCenter().getX())
+                    py = int(p.getCenter().getY())
+
+                    if nx <= px < nx + noteWidth and ny <= py < ny + noteHeight:
+                        d = (px - x) ** 2 + (py - y) ** 2
+                        if d < best_dist:
+                            best_dist = d
+                            closest_pin = (px, py)
+
+                if closest_pin:
+                    px, py = closest_pin
+                    client.sendall(f"UNPIN {px} {py}".encode())
+                    temp = False
+                    n.pinned = False
+                else:
+                    client.sendall(f"PIN {x} {y}".encode())
+                    temp = True
+                    n.pinned = True
+
+                client.recv(BUFFER)
                 redraw_board()
-                continue
-            elif (mode == 'PIN'):
-                for n in notes:
-                    if (n.clicked(x)):
-                        if (n.pinned):
-                            client.sendall(f"UNPIN {n.id}".encode('utf-8'))
-                            n.pinned = False
-                        else:
-                            client.sendall(f"PIN {n.id}".encode('utf-8'))
-                        break
-                redraw_board()
+                n.pinned = temp
+                break
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', default=DEF_HOST)
-    parser.add_argument('--port', type=int, default=DEF_PORT)
+    parser.add_argument("--host", default=DEF_HOST)
+    parser.add_argument("--port", type=int, default=DEF_PORT)
     args = parser.parse_args()
-    print(args.host)
     start_client(args.host, args.port)
--- server/server.py
+++ server/server.py
@@ -2,46 +2,262 @@
 This file implements the  server, which is mutlithreaded
 """
 
-#Spin up a server
+# Spin up a server
 import socket
 from _thread import start_new_thread
+from typing import List
 import argparse
 import sys
 import json
+from readerwriterlock import *
+from note import note
 
 DEF_HOST = '0.0.0.0'
 DEF_PORT = 5000
 BUFFER = 1024
 BACKLOG = 10
-DEF_COLOUR_SCHEME = 'gold purple'
+DEF_COLOUR_SCHEME = ['gold', 'purple']
 
 
+class board_handler:
+    def __init__(self, board_width: int, board_height: int, note_width: int, note_height: int, colours: list[str]):
+        self.board_width = board_width
+        self.board_height = board_height
+        self.note_width = note_width
+        self.note_height = note_height
+        self.reader = rwlock.RWLockRead()
+        self.writer = rwlock.RWLockWrite()
+        self.colours = set(colours)
+        self.posted_notes = {}
+        self.pins = {}
+
+    ########################### INTERNAL HELPERS############################
+    def _is_notNegative(self, value: int):
+        return value >= 0
+
+    def _point_valid(self, x: int, y: int):
+        return self._is_notNegative(x) and self._is_notNegative(y) and 0 <= x <= self.board_width and 0 <= y <= self.board_height
+
+    def _note_within_bounds(self, x: int, y: int):
+        return self._point_valid(x, y) and self._point_valid(x+self.note_width, y+self.note_height)
+
+    def _inside_note(self, n: note, px: int, py: int):
+        return (n.x <= px < n.x + self.note_width) and (n.y <= py < n.y + self.note_height)
+
+    def _note_contains_point(self, x: int, y: int):
+        affects = []
+        for k, v in self.posted_notes.items():
+            if self._inside_note(v, x, y):
+                affects.append(k)
+        return affects
+
+    ####################### Error Helpers########################
+    def _err(self, err_code: str):
+        return f"ERROR {err_code}"
+
+    def _ok(self, message: str):
+        return f"OK {message}"
+
+    def _ok_lst(self, lines: list[str]):
+        out = ["OK"]
+        out.extend(lines)
+        out.append("END")
+        return "\n".join(out)
+
+    ############################ COMMANDS############################
+
+    def POST(self, x: int, y: int, colour: str, message: str):
+        # writiing the validation code:
+        if not self._note_within_bounds(x, y):
+            return self._err("OUT_OF_BOUNDS")
+        if colour not in self.colours:
+            return self._err("INVALID_COLOUR")
+        if (x, y) in self.posted_notes:
+            return self._err("COMPLETE_OVERLAP")
+        message = (message or "").replace("\n", " ").replace("\r", " ").strip()
+        self.posted_notes[(x, y)] = note(x, y, message, colour)
+        return self._ok("NOTE_POSTED")
+
+    def PIN(self, x: int, y: int):
+        with self.writer.gen_wlock():
+            if not self._point_valid(x, y):
+                return self._err("OUT_OF_BOUNDS")
+
+            affects = self._note_contains_point(x, y)
+            if not affects:
+                return self._err("NO_NOTE")
+
+            pin = (x, y)
+            if pin not in self.pins:
+                self.pins[pin] = set()
+
+            for note_key in affects:
+                n = self.posted_notes[note_key]
+                n.add_pin(x, y)
+                self.pins[pin].add(n)
+
+            return self._ok("NOTE_PINNED")
+
+    def UNPIN(self, x: int, y: int):
+        with self.writer.gen_wlock():
+            if not self._point_valid(x, y):
+                return self._err("OUT_OF_BOUNDS")
+            pin = (x, y)
+            if pin not in self.pins:
+                return self._err("PIN_NOT_FOUND")
+
+            for n in list(self.pins[pin]):
+                if n in self.posted_notes:
+                    self.posted_notes[n].remove_pin(x, y)
+            del self.pins[pin]
+            return self._ok(f"UNPINNED_({x},{y})")
+
+    def SHAKE(self):
+        with self.writer.gen_wlock():
+            to_rm = [nk for nk, n in self.posted_notes.items()
+                     if not n.is_pinned()]
+            for n in to_rm:
+                del self.posted_notes[n]
+            return self._ok("SHAKE_COMPLETE")
+
+    def CLEAR(self):
+        with self.writer.gen_wlock():
+            self.posted_notes.clear()
+            self.pins.clear()
+            return self._ok("CLEAR_COMPLETE")
+
+    def GET_PINS(self):
+        with self.reader.gen_rlock():
+            coord = sorted(self.pins.keys())
+            lines = [f"PIN {x} {y}" for (x, y) in coord]
+            return self._ok_lst(lines)
+
+    def GET(self, colour: str = "None", contains: str = "None", refersTo: str = "None"):
+        with self.reader.gen_rlock():
+            if colour != "None" and colour not in self.colours:
+                return self._err("COLOUR_NOT_SUPPORTED")
+
+            if contains != "None":
+                x, y = contains
+
+                if not self._point_valid(int(x), int(y)):
+                    return self._err("OUT_OF_BOUNDS")
+
+            if refersTo != "None":
+                refersTo = refersTo.strip()
+
+            output = []
+            for n in self.posted_notes.values():
+                if colour != "None" and n.colour != colour:
+                    continue
+                if contains != "None":
+                    x, y = contains
+                    if not self._inside_note(n, int(x), int(y)):
+                        continue
+                if refersTo != "None" and refersTo not in n.messages:
+                    continue
+                output.append(n)
+            output.sort(key=lambda t: (t.y, t.x, t.colour, t.messages))
+            print(output)
+            return self._ok_lst([str(n) for n in output])
 
 
-def server_ops(client_socket: socket.socket, client_address: tuple, board_width: int, board_height:int, note_width:int, note_height: int, colours:str) -> None:
+def server_ops(client_socket: socket.socket, client_address: tuple,
+               board_width: int, board_height: int,
+               note_width: int, note_height: int, colours: list[str]) -> None:
+
     client_ip, client_port = client_address
-    #When a client first connects, send handshake data:
-    #handshake data set up when the server initializes. 
     print(f"Connection from {client_ip}:{client_port}")
-    data_to_send = [board_width,board_height,note_height,note_width,colours.split()]
-    message = json.dumps(data_to_send).encode('utf-8') 
-    client_socket.sendall(message)
-    
+
+    # Send handshake
+    data_to_send = [board_width, board_height,
+                    note_height, note_width, colours]
+    client_socket.sendall(json.dumps(data_to_send).encode('utf-8'))
+
+    board = board_handler(board_width, board_height,
+                          note_width, note_height, colours)
+
+    try:
+        while True:
+            data = client_socket.recv(BUFFER)
+            if not data:
+                break
+
+            command = data.decode('utf-8').strip()
+            response = handle_command(board, command)
+            client_socket.sendall(response.encode('utf-8'))
+
+    except ConnectionResetError:
+        print(f"Connection lost from {client_ip}:{client_port}")
+    finally:
+        client_socket.close()
 
 
-    
-    
-            
+def handle_command(board: board_handler, command_str: str) -> str:
+    parts = command_str.strip().split()
+    if not parts:
+        return "ERROR EMPTY_COMMAND"
 
-def main(host: str,port:int, board_width: int, board_height:int, note_width:int, note_height: int, colours:str):
-    print("ho")
-    #Setting up the server: 
+    cmd = parts[0].upper()
+
+    try:
+        if cmd == "POST":
+            x, y = int(parts[1]), int(parts[2])
+            colour = parts[3]
+            message = " ".join(parts[4:]) if len(parts) > 4 else ""
+            return board.POST(x, y, colour, message)
+
+        elif cmd == "PIN":
+            x, y = int(parts[1]), int(parts[2])
+            return board.PIN(x, y)
+
+        elif cmd == "UNPIN":
+            x, y = int(parts[1]), int(parts[2])
+            return board.UNPIN(x, y)
+
+        elif cmd == "SHAKE":
+            return board.SHAKE()
+
+        elif cmd == "CLEAR":
+            return board.CLEAR()
+
+        elif cmd == "GET_PINS":
+            return board.GET_PINS()
+
+        elif cmd == "GET":
+            # Expected format:
+            # GET <colour|None> <x|None> <y|None> <refersTo|None>
+
+            if len(parts) != 5:
+                return "ERROR INVALID_FORMAT"
+
+            colour = parts[1]
+            x = parts[2]
+            y = parts[3]
+            refersTo = parts[4]
+
+            if x == "None" and y == "None":
+                contains = "None"
+            else:
+                contains = (x, y)
+
+            return board.GET(colour, contains, refersTo)
+
+        else:
+            return "ERROR UNKNOWN_COMMAND"
+
+    except (IndexError, ValueError):
+        return "ERROR INVALID_FORMAT"
+
+
+def main(host: str, port: int, board_width: int, board_height: int, note_width: int, note_height: int, colours: str):
+    print("hi")
+    # Setting up the server:
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        # Allow reusing the socket address immediately
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        
+
         # Bind and listen
         server_socket.bind((host, port))
         server_socket.listen(BACKLOG)
@@ -51,7 +267,8 @@ def main(host: str,port:int, board_width: int, board_height:int, note_width:int,
             try:
                 print("Waiting for a connection...")
                 client_socket, client_address = server_socket.accept()
-                start_new_thread (server_ops, (client_socket,client_address,board_width,board_height,note_width,note_height,colours))
+                start_new_thread(server_ops, (client_socket, client_address,
+                                 board_width, board_height, note_width, note_height, colours))
 
             except:
                 break
@@ -60,28 +277,28 @@ def main(host: str,port:int, board_width: int, board_height:int, note_width:int,
 
     finally:
         server_socket.close()
-        #Powers down the server.
+        # Powers down the server.
 
-#Args are set by the setup command, then passed to eacb subsequent thread
+# Args are set by the setup command, then passed to eacb subsequent thread
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    #host
-    parser.add_argument("host", default = DEF_HOST)
-    #port number
-    parser.add_argument("port",type =int)
-    #Board Width
-    parser.add_argument("board_width",type= int)
-    #board height
-    parser.add_argument("board_height", type = int)
-    #note width
-    parser.add_argument("note_width", type = int)
-    #note height
-    parser.add_argument("note_height", type= int)
-    #colours
-    parser.add_argument("colours", nargs = "+")
+    # host
+    parser.add_argument("host", default=DEF_HOST)
+    # port number
+    parser.add_argument("port", type=int)
+    # Board Width
+    parser.add_argument("board_width", type=int)
+    # board height
+    parser.add_argument("board_height", type=int)
+    # note width
+    parser.add_argument("note_width", type=int)
+    # note height
+    parser.add_argument("note_height", type=int)
+    # colours
+    parser.add_argument("colours", nargs="+")
 
     args = parser.parse_args()
-    main(args.host, args.port, args.board_width, args.board_height, args.note_width, args.note_height, args.colours)
-
-
+    main(args.host, args.port, args.board_width, args.board_height,
+         args.note_width, args.note_height, args.colours)
